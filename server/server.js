27f1a0068b636137ca82c10ec4b7c42d103841ac
Jerriet Kuo <jerrietkuo@gmail.com>
2024-09-19 12:58:26 -0400

    update

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const path = require('path');
+require ("dotenv").config();
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+// setup apollo server and use typeDefs, resolvers, and auth for context
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+ context: authMiddleware,
+//  These two lines below enable the playground when deployed to Heroku.
+  introspection: true,
+  playground: true, 
+});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +30,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
